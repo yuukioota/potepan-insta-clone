5c4ea0bf99ff132aed0d214db1773d4cc5687354
EC2 Default User <ec2-user@ip-172-31-43-125.ap-northeast-1.compute.internal>
2020-04-01 16:16:41 +0000

    投稿削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: %i(show destroy)
   
   def index
-    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
+    @posts = Post.includes(:photos, :user).order('created_at DESC')
   end
   
   def new
@@ -23,11 +24,23 @@ class PostsController < ApplicationController
   end
   
   def show
-    @post = Post.find_by(id: params[:id])
+  end
+  
+  def destroy
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
   end
   
   private
     def post_params
       params.require(:post).permit(:caption,photos_attributes: [:image]).merge(user_id: current_user.id)
+    end
+    
+    def set_post
+      @post = Post.find_by(id: params[:id])
     end
 end
