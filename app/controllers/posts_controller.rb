0d26688427238ab32e4b42fd35dbb36a4af40eca
EC2 Default User <ec2-user@ip-172-31-43-125.ap-northeast-1.compute.internal>
2020-04-01 15:09:15 +0000

    投稿詳細モーダルの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,10 @@ class PostsController < ApplicationController
     end
   end
   
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
   private
     def post_params
       params.require(:post).permit(:caption,photos_attributes: [:image]).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
                         registrations: 'registrations' }
   root to: "posts#index"
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(index new create) do
+  resources :posts, only: %i(index new create show) do
     resources :photos, only: %i(create)
   end
 end
