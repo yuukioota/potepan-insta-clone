0ce4f38a8894c168c991bbb9f58223aaa86ad672
EC2 Default User <ec2-user@ip-172-31-43-125.ap-northeast-1.compute.internal>
2020-04-01 17:37:24 +0000

    いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', 
+  devise_for :users, controllers: {
+                        omniauth_callbacks: 'users/omniauth_callbacks', 
                         registrations: 'registrations' }
   root to: "posts#index"
   get '/users/:id', to: 'users#show', as: 'user'
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
   end
 end
