e2825673eecbaf0ce0a053f003517200e4f71792
EC2 Default User <ec2-user@ip-172-31-43-125.ap-northeast-1.compute.internal>
2020-04-05 02:59:45 +0000

    アカウント削除機能の実装とアカウント削除時にいいねやコメントも削除されるように修正

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!
+  
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if @user.destroy
+      flash[:notice] = "アカウントを削除しました" 
+      redirect_to new_user_registration_path
+    else
+      render edit_user_registration
+    end
+    
+  end
 
   protected
 
-  def update_resource(resource, params)
-    resource.update_without_current_password(params)
-  end
-  
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
+    def update_resource(resource, params)
+      resource.update_without_current_password(params)
+    end
+    
+    def after_update_path_for(resource)
+      user_path(resource)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,  :omniauthable
